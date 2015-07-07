b24830abd356f699c796b1ff05a6b3504e47b5c0
berg <bergtian@qq.com>
2015-07-07 17:47:43 +0800

    add raw sourcecodes

--- qcom_msm8916_64_android5.0/README.md
+++ qcom_msm8916_64_android5.0/README.md
@@ -1,2 +1,9 @@
 port TWRP to android5.0.2 for qcom msm8916_64
 
+dirs:
+
+* bootable/recovery_qcom qcom recovery,never changed
+* bootable/recovery	twrp recovery
+* build/core/		build system core
+* device/qcom/msm8916_64_qcom	qcom target device,never changed
+* device/qcom/msm8916_64	twrp target devices
---
+++ qcom_msm8916_64_android5.0/device/qcom/msm8916_64_raw/msm8916_64.mk
@@ -0,0 +1,123 @@
+DEVICE_PACKAGE_OVERLAYS := device/qcom/msm8916_64/overlay
+
+TARGET_USES_QCOM_BSP := true
+# Add QC Video Enhancements flag
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+# media_profiles and media_codecs xmls for 8916
+ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
+PRODUCT_COPY_FILES += device/qcom/msm8916_32/media/media_profiles_8916.xml:system/etc/media_profiles.xml \
+                      device/qcom/msm8916_32/media/media_codecs_8916.xml:system/etc/media_codecs.xml \
+                      device/qcom/msm8916_32/media/media_codecs_8939.xml:system/etc/media_codecs_8939.xml \
+                      device/qcom/msm8916_32/media/media_codecs_8929.xml:system/etc/media_codecs_8929.xml
+endif
+
+TARGET_USES_QCA_NFC := other
+
+PRODUCT_PROPERTY_OVERRIDES += \
+           dalvik.vm.heapgrowthlimit=128m
+$(call inherit-product, device/qcom/common/common64.mk)
+
+PRODUCT_NAME := msm8916_64
+PRODUCT_DEVICE := msm8916_64
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := MSM8916 for arm64
+
+PRODUCT_BOOT_JARS += qcmediaplayer \
+                     WfdCommon \
+                     qcom.fmradio \
+                     oem-services
+PRODUCT_BOOT_JARS += vcard
+PRODUCT_BOOT_JARS += tcmiface
+
+# default is nosdcard, S/W button enabled in resource
+PRODUCT_CHARACTERISTICS := default
+
+#Android EGL implementation
+PRODUCT_PACKAGES += libGLES_android
+
+PRODUCT_PACKAGES += \
+    libqcomvisualizer \
+    libqcompostprocbundle \
+    libqcomvoiceprocessing
+
+# Audio configuration file
+PRODUCT_COPY_FILES += \
+    device/qcom/msm8916_32/audio_policy.conf:system/etc/audio_policy.conf \
+    device/qcom/msm8916_32/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    device/qcom/msm8916_32/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml \
+    device/qcom/msm8916_32/mixer_paths_qrd_skuh.xml:system/etc/mixer_paths_qrd_skuh.xml \
+    device/qcom/msm8916_32/mixer_paths_qrd_skui.xml:system/etc/mixer_paths_qrd_skui.xml \
+    device/qcom/msm8916_32/mixer_paths_qrd_skuhf.xml:system/etc/mixer_paths_qrd_skuhf.xml \
+    device/qcom/msm8916_32/mixer_paths_wcd9306.xml:system/etc/mixer_paths_wcd9306.xml \
+    device/qcom/msm8916_32/mixer_paths_skuk.xml:system/etc/mixer_paths_skuk.xml \
+    device/qcom/msm8916_32/mixer_paths.xml:system/etc/mixer_paths.xml \
+    device/qcom/msm8916_32/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    device/qcom/msm8916_32/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
+    device/qcom/msm8916_32/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    device/qcom/msm8916_64/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
+
+
+#ANT+ stack
+PRODUCT_PACKAGES += \
+    AntHalService \
+    libantradio \
+    antradio_app
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1
+PRODUCT_PACKAGES += wcnss_service
+
+# MSM IRQ Balancer configuration file
+PRODUCT_COPY_FILES += \
+    device/qcom/msm8916_64/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
+
+#wlan driver
+PRODUCT_COPY_FILES += \
+    device/qcom/msm8916_64/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
+    device/qcom/msm8916_64/WCNSS_wlan_dictionary.dat:persist/WCNSS_wlan_dictionary.dat \
+    device/qcom/msm8916_64/WCNSS_qcom_wlan_nv.bin:persist/WCNSS_qcom_wlan_nv.bin
+
+PRODUCT_PACKAGES += \
+    wpa_supplicant_overlay.conf \
+    p2p_supplicant_overlay.conf
+
+ifeq ($(TARGET_USES_QCA_NFC),true)
+NFC_D := true
+
+ifeq ($(NFC_D), true)
+    PRODUCT_PACKAGES += \
+        libqnfc-nci \
+        libqnfc_nci_jni \
+        nfc_nci.msm8916 \
+        QNfc \
+        Tag \
+        com.android.nfc_extras \
+        com.android.nfc.helper \
+        SmartcardService \
+        org.simalliance.openmobileapi \
+        org.simalliance.openmobileapi.xml \
+        libassd
+else
+    PRODUCT_PACKAGES += \
+    libnfc-nci \
+    libnfc_nci_jni \
+    nfc_nci.msm8916 \
+    NfcNci \
+    Tag \
+    com.android.nfc_extras
+endif
+
+# file that declares the MIFARE NFC constant
+# Commands to migrate prefs from com.android.nfc3 to com.android.nfc
+# NFC access control + feature files + configuration
+PRODUCT_COPY_FILES += \
+        packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt \
+        frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
+        frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+        frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+        frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml 
+
+endif # TARGET_USES_QCA_NFC
+
+PRODUCT_COPY_FILES += \
+	device/qcom/msm8916_64/twrp.fstab:recovery/root/etc/twrp.fstab
